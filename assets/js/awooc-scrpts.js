1e6ec50a75fd54a6659e951d50edd7bc9f0f3b42
Artem Abramovich <artikus.sol@gmail.com>
2018-04-04 01:32:13 +0400

    Initial

---
+++ assets/js/awooc-scrpts.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function ($) {
+    $('.awooc-custom-order').click(function () {
+        var productVariantId = $('.variations_form').find('input[name="variation_id"]').val();
+        var productPrice = $('.variations_form').find('.woocommerce-variation-price .price').html();
+        var productPriceSimple = $('.product-type-simple').find('.price').html();
+        $('#awooc-form-custom-order').find('.awooc-form-custom-order-price').html(productPrice);
+        // console.log(productPrice);
+        // console.log(productVariantId);
+        if (productPrice != undefined) {
+            $('#awooc-form-custom-order').find('.awooc-form-custom-order-price').html(productPrice);
+        } else {
+            $('#awooc-form-custom-order').find('.awooc-form-custom-order-price').html(productPriceSimple);
+        }
+        if (productVariantId != 0 && productVariantId != undefined) {
+            var data = {
+                id: productVariantId,
+                action: 'awooc_ajax_variant_order',
+                nonce: awooc_scrpts.nonce
+            };
+            $.ajax({
+                url: awooc_scrpts.url,
+                data: data,
+                type: 'POST',
+                dataType: 'json',
+                success: function (data) {
+                    console.log(data);
+                    $('.awooc-form-custom-order-attr').text(data);
+                }
+            });
+        }
+        $('#awooc-form-custom-order').removeClass('mfp-hide');
+        $.blockUI({
+            message: $('#awooc-form-custom-order'), css: {
+                width: '100%',
+                maxWidth: '600px',
+                top: '10%',
+                left: '35%'
+            }
+        });
+
+        var productVariationsTitle = $('#awooc-form-custom-order').find('.awooc-form-custom-order-title').text();
+        $.trim(productVariationsTitle);
+        $('.awooc-hidden-title').val(productVariationsTitle);
+        $('.blockOverlay').attr('title', 'Click to unblock').click($.unblockUI);
+
+    });
+    document.addEventListener('wpcf7submit', function (event) {
+        setTimeout($.unblockUI, 3000);
+        setTimeout(function () {
+            $('.wpcf7-form')[0].reset();
+            $('.wpcf7-response-output').remove();
+        }, 3000);
+    }, false);
+});
