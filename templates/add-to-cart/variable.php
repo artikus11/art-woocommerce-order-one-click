64a23bd8565b4bd7098b48a6a172b3f3cdee532b
Артем <artikus.sol@gmail.com>
2023-01-21 22:09:55 +0400

    UPD 3.0.0: правки содержимого файлов согласно родительсяки из Вукомерса

--- templates/add-to-cart/variable.php
+++ templates/add-to-cart/variable.php
@@ -1,38 +1,85 @@
 <?php
 /**
- * Single variation cart button
+ * Variable product add to cart
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/variable.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.4.0
+ * @version 6.1.0
  */
 
 defined( 'ABSPATH' ) || exit;
 
 global $product;
-?>
-<div class="woocommerce-variation-add-to-cart variations_button">
-	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-	<?php
-	do_action( 'woocommerce_before_add_to_cart_quantity' );
+$attribute_keys  = array_keys( $attributes );
+$variations_json = wp_json_encode( $available_variations );
+$variations_attr = function_exists( 'wc_esc_json' ) ? wc_esc_json( $variations_json ) : _wp_specialchars( $variations_json, ENT_QUOTES, 'UTF-8', true );
 
-	woocommerce_quantity_input(
-		array(
-			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-		)
-	);
+do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-	do_action( 'woocommerce_after_add_to_cart_quantity' );
-	?>
+<form class="variations_form cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo $variations_attr; // WPCS: XSS ok. ?>">
+	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
-	<button type="submit" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
+		<p class="stock out-of-stock"><?php echo esc_html( apply_filters( 'woocommerce_out_of_stock_message', __( 'This product is currently out of stock and unavailable.', 'woocommerce' ) ) ); ?></p>
+	<?php else : ?>
+		<table class="variations" cellspacing="0" role="presentation">
+			<tbody>
+				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
+					<tr>
+						<th class="label"><label for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>"><?php echo wc_attribute_label( $attribute_name ); // WPCS: XSS ok. ?></label></th>
+						<td class="value">
+							<?php
+								wc_dropdown_variation_attribute_options(
+									array(
+										'options'   => $options,
+										'attribute' => $attribute_name,
+										'product'   => $product,
+									)
+								);
+								echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) ) : '';
+							?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+		<?php do_action( 'woocommerce_after_variations_table' ); ?>
 
-	<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+		<div class="single_variation_wrap">
+			<?php
+				/**
+				 * Hook: woocommerce_before_single_variation.
+				 */
+				do_action( 'woocommerce_before_single_variation' );
 
-	<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
-	<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
-	<input type="hidden" name="variation_id" class="variation_id" value="0" />
-</div>
+				/**
+				 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
+				 *
+				 * @since 2.4.0
+				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+				 */
+				do_action( 'woocommerce_single_variation' );
+
+				/**
+				 * Hook: woocommerce_after_single_variation.
+				 */
+				do_action( 'woocommerce_after_single_variation' );
+			?>
+		</div>
+	<?php endif; ?>
+
+	<?php do_action( 'woocommerce_after_variations_form' ); ?>
+</form>
+
+<?php
+do_action( 'woocommerce_after_add_to_cart_form' );
