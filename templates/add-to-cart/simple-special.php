b663250319a0cbb13a2a2fd7eb82b8a8ae70a6cb
Артем <artikus.sol@gmail.com>
2023-01-28 02:22:19 +0400

    UPD 3.0.0: правки логики вывода кнопки в специальном режиме для простых товаров

--- templates/add-to-cart/simple-special.php
+++ templates/add-to-cart/simple-special.php
@@ -46,7 +46,7 @@ if ( $product->is_on_backorder() || $product->is_in_stock() ) {
 	do_action( 'woocommerce_after_add_to_cart_quantity' );
 	?>
 
-	<?php if ( $product->is_in_stock() && $product->is_purchasable() ): ?>
+	<?php if ( $product->is_in_stock() && $product->get_price() > 0 ): ?>
 		<button type="submit"
 		        name="add-to-cart"
 		        value="<?php echo esc_attr( $product->get_id() ); ?>"
